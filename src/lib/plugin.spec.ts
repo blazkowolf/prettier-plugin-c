915a1667b9bac630e3f3a68e3b918b2196a44b2a
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2024-09-23 22:44:29 -0400

    feat: get through hello world code sample

--- src/lib/plugin.spec.ts
+++ src/lib/plugin.spec.ts
@@ -9,35 +9,19 @@ int main(int argc, char *argv[]) {
   printf("Hello, World!");
   return 0;
 }`;
+//     const expected = `#include "stdio.h"
+//
+// // Some code here
+//
+// int main(int argc, char *argv[]) {
+//   printf("Hello, World!");
+//
+//   return 0;
+// }`;
+
     const actual = await format(expected, { parser: 'c', plugins: [plugin] });
 
     expect(actual).toEqual(expected);
-    // (translation_unit
-    //   (preproc_include path: (system_lib_string))
-    //   (function_definition
-    //     type: (primitive_type)
-    //     declarator:
-    //       (function_declarator
-    //         declarator: (identifier)
-    //         parameters:
-    //           (parameter_list
-    //             (parameter_declaration
-    //               type: (primitive_type)
-    //               declarator: (identifier))
-    //             (parameter_declaration
-    //               type: (primitive_type)
-    //               declarator:
-    //                 (pointer_declarator
-    //                   declarator: (array_declarator declarator: (identifier))))))
-    //     body:
-    //       (compound_statement
-    //         (expression_statement
-    //           (call_expression
-    //             function: (identifier)
-    //             arguments:
-    //               (argument_list
-    //                 (string_literal (string_content)))))
-    //         (return_statement (number_literal)))))
   });
 
   it.skip('function_call', async () => {
--- src/lib/printer.ts
+++ src/lib/printer.ts
@@ -4,11 +4,17 @@ import { doc, type Printer } from 'prettier';
 const { group, indent, join, line, softline, hardline } = doc.builders;
 
 // const BACK_SLASH = '\\';
+const ASTERISK = '*';
 const COMMA = ',';
 const DOUBLE_QUOTE = '"';
 const INCLUDE = '#include';
 const LEFT_PAREN = '(';
 const RIGHT_PAREN = ')';
+const LEFT_BRACKET = '[';
+const RIGHT_BRACKET = ']';
+const LEFT_BRACE = '{';
+const RIGHT_BRACE = '}';
+const RETURN = 'return';
 const SEMICOLON = ';';
 const SPACE = ' ';
 
@@ -28,13 +34,37 @@ export const print: Printer<SyntaxNode>['print'] = (
     // Non-terminals
     case 'translation_unit':
     case 'call_expression':
+    case 'function_definition':
+    case 'function_declarator':
+    case 'parameter_declaration':
       return path.map(print, 'namedChildren');
+
     case 'preproc_include':
       return [INCLUDE, SPACE, path.call(print, 'firstNamedChild'), hardline];
+
     case 'expression_statement':
       return [path.call(print, 'firstNamedChild'), SEMICOLON];
+
+    case 'return_statement':
+      return [RETURN, SPACE, path.call(print, 'firstNamedChild'), SEMICOLON];
+
+    case 'compound_statement':
+      return [
+        LEFT_BRACE,
+        indent([hardline, join(hardline, path.map(print, 'namedChildren'))]),
+        hardline,
+        RIGHT_BRACE,
+      ];
+
     case 'string_literal':
       return [DOUBLE_QUOTE, path.call(print, 'firstNamedChild'), DOUBLE_QUOTE];
+
+    case 'pointer_declarator':
+      return [ASTERISK, path.call(print, 'firstNamedChild')];
+
+    case 'array_declarator':
+      return [path.call(print, 'firstNamedChild'), LEFT_BRACKET, RIGHT_BRACKET];
+
     case 'argument_list':
       return group([
         LEFT_PAREN,
@@ -46,10 +76,26 @@ export const print: Printer<SyntaxNode>['print'] = (
         RIGHT_PAREN,
       ]);
 
+    case 'parameter_list':
+      return group([
+        LEFT_PAREN,
+        indent([
+          softline,
+          join([COMMA, line], path.map(print, 'namedChildren')),
+        ]),
+        softline,
+        RIGHT_PAREN,
+        SPACE,
+      ]);
+
     // Terminals
     case 'comment':
-    case 'system_lib_string':
+      return [node.text, hardline];
+
     case 'primitive_type':
+      return [node.text, SPACE];
+
+    case 'system_lib_string':
     case 'identifier':
     case 'string_content':
     case 'number_literal':
